2ef334c73c2da50752d85b83f8a15b1957c1cb15
davesalamangkero28 <dave.salamanca@holychild.edu.ph>
2026-03-31 15:21:52 +0800

    3/31/26 3:21 PM Changes

--- movies/create.php
+++ movies/create.php
@@ -26,7 +26,7 @@
                         <input type="number" class="form-control" id="release_year" name="release_year" placeholder="e.g. 2023" min="1900" max="2025" required>
                     </div>
                     <div class="mb-3">
-                        <label for="rental_rate" class="form-label">Rental Rate ($)</label>
+                        <label for="rental_rate" class="form-label">Rental Rate  (₱)</label>
                         <input type="number" step="0.01" class="form-control" id="rental_rate" name="rental_rate" placeholder="e.g. 4.99" min="0" required>
                     </div>
                     <button type="submit" class="btn btn-primary w-100">Save Movie</button>
