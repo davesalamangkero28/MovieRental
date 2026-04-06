da7b5d4cad6ffc3bd2abaf8cd0e65a7edd1c9597
davesalamangkero28 <dave.salamanca@holychild.edu.ph>
2026-04-06 08:22:46 +0800

    04/06/26

--- config/connect.php
+++ config/connect.php
@@ -7,4 +7,8 @@ $conn = mysqli_connect($host, $username, $password, $database);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+<<<<<<< Updated upstream
 ?>
+=======
+?>
+>>>>>>> Stashed changes
