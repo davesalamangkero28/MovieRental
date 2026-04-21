8908180e3db3dbc8c22ed652fa855a135c90853d
davesalamangkero28 <dave.salamanca@holychild.edu.ph>
2026-04-21 12:38:41 +0800

    4/21/2026 changes

--- index.php
+++ index.php
@@ -9,45 +9,43 @@
 </head>
 
 <body>
-<header>
-<div class="sidebar">
-    <div class="logo">🎬 MovieDashboard</div>
 
-</div>
+<div class="layout">
 
-    <div class="sidebar-menu">
-        <a href="index.php">Home</a>
-        <a href="#">Payments</a>
-        <a href="movies/index.php">Movies</a>
-        <a href="customers/index.php">Customers</a>
-        <a href="rentals/index.php">Rentals</a>
-    </div>
-</header>
+    <div class="sidebar">
+        <div class="logo">🎬MovieDashboard</div>
 
-<div class="container">
-
-    <h1 class="section-title">Movies For Rent</h1>
-
-    <div class="movie-list">
-
-    <?php
-    $result = mysqli_query($conn, "SELECT * FROM movies");
-
-    while ($row = mysqli_fetch_assoc($result)) {
-    ?>
-
-        <div class="movie-card">
-            <img src="images/movies/<?php echo $row['image']; ?>" alt="movie poster">
-
-            <div class="overlay">
-                <h3><?php echo $row['title']; ?></h3>
-                <p><?php echo $row['genre']; ?></p>
-                <span><?php echo $row['release_year']; ?></span>
-            </div>
+        <div class="sidebar-menu">
+            <a href="index.php" class="active">Home</a>
+            <a href="#">Payments</a>
+            <a href="movies/index.php">Movies</a>
+            <a href="customers/index.php">Customers</a>
+            <a href="rentals/index.php">Rentals</a>
         </div>
+    </div>
 
-    <?php } ?>
+    <div class="container">
+        <h1 class="section-title">Movies For Rent</h1>
 
+        <div class="movie-list">
+            <?php
+            $result = mysqli_query($conn, "SELECT * FROM movies");
+
+            while ($row = mysqli_fetch_assoc($result)) {
+            ?>
+                <div class="movie-card">
+                    <img src="images/movies/<?php echo $row['image']; ?>" alt="movie poster">
+
+                    <div class="overlay">
+                        <h3><?php echo $row['title']; ?></h3>
+                        <p><?php echo $row['genre']; ?></p>
+                        <span><?php echo $row['release_year']; ?></span>
+
+                        <a href="rentals/index.php" class="rent-btn">Rent</a>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
     </div>
 
 </div>
